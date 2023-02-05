65a6269b460d5930979e7e1100818e8dc26916bd
abelTomas73 <67728381+abelTomas73@users.noreply.github.com>
2023-02-05 18:05:04 +0100

    Add files via upload

---
+++ mightyweb-alter-table.sql
@@ -0,0 +1,1 @@
+INSERT INTO `right_header_icon` (`id`, `title`, `value`, `image`, `type`, `url`, `status`, `created_at`, `updated_at`) VALUES (NULL, 'QR Code', 'ic_qr_code', 'ic_qr_code.png', 'event', NULL, '0', '2022-07-25 05:10:36', NULL);
